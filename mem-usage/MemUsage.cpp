1c1b6d2a181b13f035d0cde826a377618a61b694
Alexander Wilson <acwilson96@gmail.com>
2018-06-05 15:35:27 +0100

    Run pass on Module, print Global Vars

--- mem-usage/MemUsage.cpp
+++ mem-usage/MemUsage.cpp
@@ -1,26 +1,43 @@
-#define DEBUG_TYPE "skeletonpass"
 #include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/InstIterator.h"
-#include <map>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <iostream>
+
 using namespace llvm;
 using namespace std;
 namespace {
 
-    struct MemoryUsage : public FunctionPass {
-        
+    struct MemoryUsage : public ModulePass {
+
         static char ID;
-        MemoryUsage() : FunctionPass(ID) {}
-        virtual bool runOnFunction(Function &F) {
-            errs() << F.getName() << "\n";
+        MemoryUsage() : ModulePass(ID) {}
+        virtual bool runOnModule(Module &M) {
+            SmallVector<Value*, 64> globalVariables;
+
+            errs() << "In a Module called " << M.getName() << "\n";
+            
+            errs() << "Looking at Global Variables" << "\n";
+            errs() << "\e[1m" << "\tName\t\tSize" << "\e[0m" << "\n";
+            for (GlobalVariable &Global : M.getGlobalList()) {
+                errs() << "\t" << Global.getName() << "\t\t" << *Global.getValueType() << "\n";
+            }
+
+            errs() << "Looking at Functions:" << "\n";
+            for (Function &Function: M.getFunctionList()) {
+                errs() << "\t - " << Function.getName() << "\n"; 
+            }
+
             return false;
+        }
+
+
+        virtual int runOnFunction(Function &F) {
+            errs() << "\tIn a Function called " << F.getName() << "\n";
+            return 0;
         }
 
     };
