f303020c85767ba279e9df4a46dfe00128d734b5
Alexander Wilson <acwilson96@gmail.com>
2018-06-07 15:17:02 +0100

    Added function to return Global Variable Usage

--- mem-usage/MemUsage.cpp
+++ mem-usage/MemUsage.cpp
@@ -20,26 +20,36 @@ namespace {
 
             errs() << "In a Module called " << M.getName() << "\n";
             
-            errs() << "\nLooking at Global Variables" << "\n";
-            errs() << "\e[1m" << "\tVariable Name\t\t\tSize (Bits)" << "\e[0m" << "\n";
-            for (GlobalVariable &Global : M.getGlobalList()) {
-                string gVarName = Global.getName();
-                if (Global.getValueType()->isArrayTy()) {
-                    errs() << "\t" << gVarName << getTabs(gVarName);
-                    errs() << (Global.getValueType()->getArrayElementType()->getScalarSizeInBits() * Global.getValueType()->getArrayNumElements()) << "\t- " << *Global.getValueType();
-                }
-                else {
-                    errs() << "\t" << gVarName << getTabs(gVarName) << Global.getValueType()->getScalarSizeInBits() << "\t- " << *Global.getValueType();
-                }
-                errs() << "\n";
-            }
-
+            int globalUsage = getGlobalsUsage(M);
+            
             errs() << "\nLooking at Functions:" << "\n";
             for (Function &Function: M.getFunctionList()) {
                 errs() << "\t - " << Function.getName() << "\n"; 
             }
 
             return false;
+        }
+
+        int getGlobalsUsage(Module &M) {
+            int output = 0;
+            errs() << "\nLooking at Global Variables" << "\n";
+            errs() << "\e[1m" << "\tVariable Name\t\t\tSize (Bits)" << "\e[0m" << "\n";
+            for (GlobalVariable &Global : M.getGlobalList()) {
+                string gVarName = Global.getName();
+                if (Global.getValueType()->isArrayTy()) {
+                    int usage = (Global.getValueType()->getArrayElementType()->getScalarSizeInBits() * Global.getValueType()->getArrayNumElements());
+                    errs() << "\t" << gVarName << getTabs(gVarName) << usage << "\t- " << *Global.getValueType();
+                    output += usage;
+                }
+                else {
+                    int usage = Global.getValueType()->getScalarSizeInBits();
+                    errs() << "\t" << gVarName << getTabs(gVarName) << usage << "\t- " << *Global.getValueType();
+                    output += usage;
+                }
+                errs() << "\n";
+            }
+            errs() << "\nGlobal Usage: " << output << " Bits\n";
+            return output;
         }
 
         virtual int runOnFunction(Function &F) {
