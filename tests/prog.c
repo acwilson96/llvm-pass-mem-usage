73314a37215cc568c78fe670f26f0761e9f984f5
Alexander Wilson <acwilson96@gmail.com>
2018-06-14 22:05:41 +0100

    Standard Test

--- tests/prog.c
+++ tests/prog.c
@@ -3,8 +3,16 @@
 char char8bits;
 int  int32bits;
 
+int myFunc() {
+    int x = 5;
+    int y = 10;
+    return x + y;
+}
+
 int main() {
     char8bits = 'a';
     int32bits = 32;
+    int local32bits = 5;
+    int funcRet     = myFunc();
     printf("string length 16");
 }
